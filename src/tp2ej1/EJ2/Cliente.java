45981b92cc04fb85b8ce77f77b23b1735ed4f7eb
Neielmonti <neielmonti@gmail.com>
2023-09-30 00:04:50 -0300

    Initial commit

---
+++ src/tp2ej1/EJ2/Cliente.java
@@ -0,0 +1,34 @@
+package tp2ej1.EJ2;
+
+
+public class Cliente {
+	private int id;
+	private String razonSocial;
+	
+	//Constructores
+	public Cliente(){}
+
+	public Cliente(int id){
+		setId(id);
+		setRazonSocial(null);
+	}
+
+	public Cliente(int id, String razonSocial){
+		setId(id);
+		setRazonSocial(razonSocial);
+	}
+	
+	//Setters
+	public void setId(int id) { this.id = id;}
+	public void setRazonSocial(String descr) {this.razonSocial = descr;}
+	
+	//Getters
+	public int getId() { return id;}
+	public String getRazonSocial() {return razonSocial;}
+	
+	@Override
+	public String toString() {
+		return "Id:  " +getId()+ "\n"+"  Razon Social: "+getRazonSocial() ;
+	}
+}
+
---
+++ src/tp2ej1/EJ2/Estado.java
@@ -0,0 +1,5 @@
+package tp2ej1.EJ2;
+
+public enum Estado {
+	INICIALIZADA,FINALIZADA
+}
